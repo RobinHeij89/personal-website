d132bef0815f35d946ed526e69f05a411442aafe
Robin Heij <rooobiin89@gmail.com>
2024-04-29 07:33:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,138 @@
+import reactLogo from './assets/react.svg'
+import './global.css'
+import style from './App.module.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <div>
+          <img src={reactLogo} alt="React Logo" />
+          <h1>
+            <p>Robin Heij</p>
+            <p>Freelance developer</p>
+          </h1>
+          <p>Based in Rotterdam, the Netherlands</p>
+        </div>
+
+        <div>
+          <h2>
+            <span>
+              Conscious
+            </span> Creative Developer
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis dapibus sapien. Duis ac erat ac ligula facilisis convallis ac et ante. Mauris nec leo nibh. Aliquam erat volutpat. Fusce lorem lacus, lacinia pellentesque eleifend at, egestas et purus. Maecenas at facilisis diam, vitae hendrerit velit. Morbi risus felis, varius nec nisi id, mollis fringilla mauris. Nullam sit amet sem urna. Donec a aliquam eros. Duis sit amet massa id ligula ultricies mollis at nec sapien. Proin tincidunt finibus lectus sit amet consectetur. Etiam fringilla malesuada orci ac porta.
+          </p>
+        </div>
+
+        <canvas>
+          Your browser does not support the HTML5 canvas tag.
+        </canvas>
+
+        <div>
+          Robinheij.dev
+        </div>
+
+        <div>
+          Robinheij.dev
+        </div>
+
+        <img src='https://placekitten.com/200/300' alt="Photo" />
+      </header>
+      <main>
+        <div>
+          <div>
+            <h2>
+              <span>
+                Levering my skills
+              </span>
+              for positive change.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis dapibus sapien. Duis ac erat ac ligula facilisis convallis ac et ante. Mauris nec leo nibh.
+            </p>
+          </div>
+          <div>
+            <img src='https://placekitten.com/200/300' alt="Photo" />
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis dapibus sapien. Duis ac erat ac ligula facilisis convallis ac et ante. Mauris nec leo nibh. Aliquam erat volutpat. Fusce lorem lacus, lacinia pellentesque eleifend at, egestas et purus. Maecenas at facilisis diam, vitae hendrerit velit. Morbi risus felis, varius nec nisi id, mollis fringilla mauris. Nullam sit amet sem urna. Donec a aliquam eros. Duis sit amet massa id ligula ultricies mollis at nec sapien. Proin tincidunt finibus lectus sit amet consectetur. Etiam fringilla malesuada orci ac porta.
+            </p>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <img src='https://placekitten.com/200/300' alt="Photo" />
+            </li>
+            <li>
+              <img src='https://placekitten.com/200/300' alt="Photo" />
+            </li>
+            <li>
+              <img src='https://placekitten.com/200/300' alt="Photo" />
+            </li>
+            <li>
+              <img src='https://placekitten.com/200/300' alt="Photo" />
+            </li>
+            <li>
+              <img src='https://placekitten.com/200/300' alt="Photo" />
+            </li>
+            <li>
+              <img src='https://placekitten.com/200/300' alt="Photo" />
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>The Blog</h2>
+          <ul>
+            <li>
+              <div>
+                <p>Caption</p>
+                <h3>Title</h3>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>Caption</p>
+                <h3>Title</h3>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>Caption</p>
+                <h3>Title</h3>
+              </div>
+            </li>
+          </ul>
+          <button>More</button>
+        </div>
+
+      </main>
+      <footer>
+        <img src='https://placekitten.com/200/300' alt="Photo" />
+        <ul>
+          <li>
+            <p>Robin Heij</p>
+          </li>
+
+          <li>
+            <p>Freelance Developer</p>
+          </li>
+
+          <li>
+            <p>Based in Rotterdam, the Netherlands</p>
+          </li>
+
+          <li>
+            <p>Copyright</p>
+          </li>
+        </ul>
+      </footer>
+    </>
+  )
+}
+
+export default App
